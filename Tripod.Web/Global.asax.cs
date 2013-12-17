a7e50ae36a4a1747f2f4aafcc84a9756e1a0bf7b
windowslive@danludwig.com <windowslive@danludwig.com>
2013-12-17 00:28:30 -0500

    Move Startup file & disable MVC response headers.

--- Tripod.Web/Global.asax.cs
+++ Tripod.Web/Global.asax.cs
@@ -15,6 +15,7 @@ namespace Tripod.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MvcHandler.DisableMvcResponseHeader = true;
         }
     }
 }
