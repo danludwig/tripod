8cb0211b3a43a6f34e4c8256fb37757eb96f6e41
danludwig <windowslive@danludwig.com>
2014-03-16 13:06:59 -0400

    Create domain view and query for remote memberships to retrieve by user id.

---
+++ Tripod.Domain/Domain/Security/Queries/RemoteMembershipViewsBy.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tripod.Domain.Security
+{
+    public class RemoteMembershipViewsBy : BaseEnumerableQuery<RemoteMembershipView>, IDefineQuery<Task<IQueryable<RemoteMembershipView>>>
+    {
+        [UsedImplicitly]
+        public RemoteMembershipViewsBy(int userId)
+        {
+            UserId = userId;
+        }
+
+        public int UserId { get; private set; }
+    }
+
+    [UsedImplicitly]
+    public class HandleRemoteMembershipViewsByQuery : IHandleQuery<RemoteMembershipViewsBy, Task<IQueryable<RemoteMembershipView>>>
+    {
+        private readonly IReadEntities _entities;
+
+        public HandleRemoteMembershipViewsByQuery(IReadEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public Task<IQueryable<RemoteMembershipView>> Handle(RemoteMembershipViewsBy query)
+        {
+            var queryable = _entities.Query<RemoteMembership>().ByUserId(query.UserId);
+
+            var projection = queryable
+                .Select(x => new RemoteMembershipView
+                {
+                    Provider = x.LoginProvider,
+                    Key = x.ProviderKey,
+                    UserId = x.UserId,
+                })
+                .OrderBy(query.OrderBy);
+
+            return Task.FromResult(projection);
+        }
+    }
+}
---
+++ Tripod.Domain/Domain/Security/Views/RemoteMembershipView.cs
@@ -0,0 +1,9 @@
+namespace Tripod.Domain.Security
+{
+    public class RemoteMembershipView
+    {
+        public string Provider { get; set; }
+        public string Key { get; set; }
+        public int? UserId { get; set; }
+    }
+}
