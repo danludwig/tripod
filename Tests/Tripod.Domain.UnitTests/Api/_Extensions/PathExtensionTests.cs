ebb86cf1731c58df7ac06acee1d15a880006e397
danludwig <windowslive@danludwig.com>
2014-10-12 16:50:26 -0400

    Unit test PathExtensions.GetFullPath

---
+++ Tests/Tripod.Domain.UnitTests/Api/_Extensions/PathExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Should;
+using Xunit;
+
+namespace Tripod
+{
+    public class PathExtensionTests
+    {
+        [Fact]
+        public void GetFullPath_GetsFullPath_BasedOnAppDomainBaseDirectory()
+        {
+            const string relativePath = "App_Data/path/to/something.ext";
+            var fullPath = AppDomain.CurrentDomain.GetFullPath(relativePath);
+            var expected = string.Format(@"{0}\{1}",
+                AppDomain.CurrentDomain.BaseDirectory,
+                relativePath.Replace("/", @"\"));
+            fullPath.ShouldEqual(expected);
+        }
+    }
+}
