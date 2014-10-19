67c04be9ec00fb1151d56c27cf537a9a7396ef71
danludwig <windowslive@danludwig.com>
2014-10-19 19:50:04 -0400

    Unit test EmailMessage entity.

---
+++ Tests/Tripod.Domain.UnitTests/Domain/Security/Entities/EmailMessageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Should;
+using Xunit;
+
+namespace Tripod.Domain.Security
+{
+    public class EmailMessageTests
+    {
+        [Fact]
+        public void Ctor_InitializesNothing()
+        {
+            var entity = new EmailMessage();
+            entity.Id.ShouldEqual(0);
+            entity.EmailAddressId.ShouldEqual(0);
+            entity.EmailAddress.ShouldBeNull();
+            entity.From.ShouldBeNull();
+            entity.Subject.ShouldBeNull();
+            entity.Body.ShouldBeNull();
+            entity.IsBodyHtml.ShouldBeFalse();
+            entity.SendOnUtc.ShouldEqual(DateTime.MinValue);
+            entity.SentOnUtc.ShouldBeNull();
+            entity.CancelledOnUtc.ShouldBeNull();
+            entity.LastSendError.ShouldBeNull();
+        }
+    }
+}
