d3579235263c9e0620146efd4754e0b892fa2ce7
danludwig <windowslive@danludwig.com>
2014-10-25 14:56:31 -0400

    Unit test QueryEmailAddresses ByValue extension methods.

---
+++ Tests/Tripod.Domain.UnitTests/Domain/Security/_Extensions/QueryEmailAddressesTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Tripod.Domain.Security
+{
+    public class QueryEmailAddressesTests
+    {
+        #region ByValue
+
+        [Fact]
+        public void ByValue_Queryable_CanAllowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            data.AsQueryable().ByValue(string.Format("{0}@domain.tld", Guid.NewGuid())).ShouldBeNull();
+        }
+
+        [Fact]
+        public void ByValue_Queryable_CanDisallowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            data.AsQueryable().ByValue(data[0].Value, false).ShouldNotBeNull();
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                data.AsQueryable().ByValue(string.Format("{0}@domain.tld", Guid.NewGuid()), false));
+            Assert.NotNull(exception);
+            exception.Message.IndexOf("Sequence contains no matching element", StringComparison.CurrentCulture)
+                .ShouldEqual(0);
+        }
+
+        [Fact]
+        public void ByValue_Enumerable_CanAllowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            data.AsEnumerable().ByValue(string.Format("{0}@domain.tld", Guid.NewGuid())).ShouldBeNull();
+        }
+
+        [Fact]
+        public void ByValue_Enumerable_CanDisallowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            data.AsEnumerable().ByValue(data[0].Value, false).ShouldNotBeNull();
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                data.AsEnumerable().ByValue(string.Format("{0}@domain.tld", Guid.NewGuid()), false));
+            Assert.NotNull(exception);
+            exception.Message.IndexOf("Sequence contains no matching element", StringComparison.CurrentCulture)
+                .ShouldEqual(0);
+        }
+
+        [Fact]
+        public void ByValueAsync_Queryable_CanAllowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            var dbSet = new Mock<DbSet<EmailAddress>>(MockBehavior.Strict).SetupDataAsync(data.AsQueryable());
+            dbSet.Object.AsQueryable().ByValueAsync(string.Format("{0}@domain.tld", Guid.NewGuid()))
+                .Result.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ByValueAsync_Queryable_CanDisallowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            var dbSet = new Mock<DbSet<EmailAddress>>(MockBehavior.Strict).SetupDataAsync(data.AsQueryable());
+            dbSet.Object.AsQueryable().ByValueAsync(data[0].Value, false).Result.ShouldNotBeNull();
+
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                dbSet.Object.AsQueryable().ByValueAsync(string.Format("{0}@domain.tld", Guid.NewGuid()), false).Result);
+            Assert.NotNull(exception);
+            exception.Message.IndexOf("Sequence contains no matching element",StringComparison.CurrentCulture)
+                .ShouldEqual(0);
+        }
+
+        [Fact]
+        public void ByValueAsync_Enumerable_CanAllowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            var dbSet = new Mock<DbSet<EmailAddress>>(MockBehavior.Strict).SetupDataAsync(data.AsQueryable());
+            dbSet.Object.AsEnumerable().ByValueAsync(string.Format("{0}@domain.tld", Guid.NewGuid()))
+                .Result.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ByValueAsync_Enumerable_CanDisallowNull()
+        {
+            var data = new[]
+            {
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+                new EmailAddress { Value = string.Format("{0}@domain.tld", Guid.NewGuid()), },
+            };
+            var dbSet = new Mock<DbSet<EmailAddress>>(MockBehavior.Strict).SetupDataAsync(data.AsQueryable());
+            dbSet.Object.AsEnumerable().ByValueAsync(data[0].Value, false).Result.ShouldNotBeNull();
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                dbSet.Object.AsEnumerable().ByValueAsync(string.Format("{0}@domain.tld", Guid.NewGuid()), false).Result);
+            Assert.NotNull(exception);
+            exception.Message.IndexOf("Sequence contains no matching element", StringComparison.CurrentCulture)
+                .ShouldEqual(0);
+        }
+
+        #endregion
+        #region ByUserId
+
+        #endregion
+    }
+}
