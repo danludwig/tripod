25c481f01cceb198de2f72b9902b967507e2c737
danludwig <windowslive@danludwig.com>
2014-10-25 19:45:29 -0400

    Remove unnecessary using statements.

--- Tests/Tripod.Domain.UnitTests/Domain/Security/Queries/RemoteMembershipViewsByTests.cs
+++ Tests/Tripod.Domain.UnitTests/Domain/Security/Queries/RemoteMembershipViewsByTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using Moq;
 using Should;
--- Tests/Tripod.Domain.UnitTests/Domain/Security/Queries/UserViewByTests.cs
+++ Tests/Tripod.Domain.UnitTests/Domain/Security/Queries/UserViewByTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
